a1ed06424c80c5952099d4d1d10b64540f894814
Mingyang Song <s@ss-MacBook-Air.local>
2014-02-04 17:55:17 -0500

    new question

--- subsetsWithDup.java
+++ subsetsWithDup.java
@@ -1,22 +1,20 @@
 public class Solution {
     public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num) {
         Arrays.sort(num);
-        ArrayList<ArrayList<Integer>> res=subsetsWithDup(num, 0, new ArrayList<Integer>());
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        subsetsWithDup(num, 0, new ArrayList<Integer>(), res);
         res.add(new ArrayList<Integer>());
         return res;
     }
     
-    public ArrayList<ArrayList<Integer>> subsetsWithDup(int[] num, int index, ArrayList<Integer> cur){
-        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-        
+    public void subsetsWithDup(int[] num, int index, ArrayList<Integer> cur, ArrayList<ArrayList<Integer>> res){
         for(int i=index;i<num.length;i++){
             cur.add(num[i]);
             res.add(new ArrayList<Integer>(cur));
-            res.addAll(subsetsWithDup(num, i+1, cur));
+            subsetsWithDup(num, i+1, cur, res);
             cur.remove(cur.size()-1);
             while(i<num.length-1&&num[i+1]==num[i])
                 i++;
         }
-        return res;
     }
 }
